3e012608686b5a29e016bec4fa9b6f45f6de8751
alextaylor <alextaylor515@gmail.com>
2022-06-15 13:59:54 -0400

    commit tree a1aed74a85c2b97a9ec94c769d63e8d413097fa0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink
+    }
+}
+class Lunch {
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner extends Lunch{
+    #dessert;
+    constructor(salad,soup,entree,dessert) {
+        super(salad,soup);
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+
+let dinner = new Dinner('cesar','chicken','vodka','steak','ice cream')
